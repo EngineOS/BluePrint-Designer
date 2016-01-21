a7828499f6a9c43b6c1519ad4c0d6b9425b82efd
Lachlan Douglas <lachlan@engines.org>
2016-01-21 11:29:10 +1100

    service def debug

--- app/controllers/repository_load_service_definitions_controller.rb
+++ app/controllers/repository_load_service_definitions_controller.rb
@@ -2,7 +2,10 @@ class RepositoryLoadServiceDefinitionsController < ApplicationController
 
   def show
     ServiceDefinition.destroy_all
-    ServiceDefinition.create ::Repository::Repository.new.all_service_definition_attributes
+
+    service_definitions = ::Repository::Repository.new.all_service_definition_attributes
+    p service_definitions
+    ServiceDefinition.create service_definitions
     redirect_to service_definitions_path, notice: 'Service definitions have been loaded.'
   end
 
--- app/controllers/repository_service_definitions_controller.rb
+++ app/controllers/repository_service_definitions_controller.rb
@@ -1,7 +1,7 @@
 class RepositoryServiceDefinitionsController < ApplicationController
 
   def show
-    @repository_service_definition = ::Repository::ServiceDefinition::Load.new(params[:id]).service_definition
+    @repository_service_definition = ::Repository::ServiceDefinition::Load.new(params[:id].gsub('\\', '/')).service_definition
   end
 
 end
